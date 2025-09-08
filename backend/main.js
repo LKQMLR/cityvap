55f00d1270c45ab3335219a874e5174245a8ef47
LKQMLR.dev <lkqmlr.dev@gmail.com>
2025-09-08 09:31:35 +0200

    test

--- backend/main.js
+++ backend/main.js
@@ -1,4 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 
+// GERE LES VARIABLES D'ENVIRONNEMENT
 dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+
+app.listen(PORT, () => console.log(`En attente de requêtes sur le port : ${PORT}`))
